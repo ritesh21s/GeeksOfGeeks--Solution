bedeac7d847b4b8a526366afa84f9891a72a2e11
ritesh21s <rsingh212125@gmail.com>
2026-08-25 19:07:45 +0530

    ✅ Sum of Natural Numbers | Easy | C++ | Runtime: N/A | Memory: N/A | 2026-08-25

---
+++ gfg/easy/reverse-coding2452/solution.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int sum=0;
+    for(int i=n; i>0; i--){
+    sum += i;   
+    }
+    cout<< sum;
+    
+    
+
+    return 0;
+}
