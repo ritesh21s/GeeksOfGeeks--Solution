93bd1bed827b2c5118dc34b6dfb8933fd685a3e1
ritesh21s <rsingh212125@gmail.com>
2026-08-26 23:14:52 +0530

    ✅ All Divisors of a Number | Easy | C++ | Runtime: N/A | Memory: N/A | 2026-08-26

---
+++ gfg/easy/all-divisors-of-a-number/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    vector<int> getDivisors(int n) {
+        vector<int> v;
+        for(int i=1; i<=sqrt(n); i++){
+            if(n%i == 0){
+                v.push_back(i) ;
+                if((n/i)!= i){
+                    v.push_back(n/i);
+            }
+            }
+           
+        }
+        sort(v.begin(), v.end());
+        for(auto it: v)
+        return v;
+        
+    }
+};
