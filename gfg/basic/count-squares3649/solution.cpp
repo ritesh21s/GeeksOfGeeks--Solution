9c5d6b048474e1268b698e41c69a071f1e93eb4e
ritesh21s <rsingh212125@gmail.com>
2026-08-28 18:50:03 +0530

    ✅ Count Perfect Squares | Basic | C++ | Runtime: N/A | Memory: N/A | 2026-08-28

---
+++ gfg/basic/count-squares3649/solution.cpp
@@ -0,0 +1,10 @@
+class Solution {
+  public:
+    int countSquares(int n) {
+        int count = 0;
+        for(int i=1; i*i<n; i++){
+            count++;
+        }
+        return count;
+    }
+};
